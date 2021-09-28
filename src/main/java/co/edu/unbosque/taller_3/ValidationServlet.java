f9ebacfce03a865628d38b60185b1816ac6436c6
David Real <81327257+DavidReal04@users.noreply.github.com>
2021-09-28 17:55:05 -0500

    Initial Log in

---
+++ src/main/java/co/edu/unbosque/taller_3/ValidationServlet.java
@@ -0,0 +1,27 @@
+package co.edu.unbosque.taller_3;
+
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "validationServlet", value = "/validation-servlet")
+public class ValidationServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Ingreso exitoso";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
