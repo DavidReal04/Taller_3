7af4ce77ef7d4bbe91e764d6a323c9c8301a05d7
David Real <81327257+DavidReal04@users.noreply.github.com>
2021-10-01 13:00:37 -0500

    Update DB & Validation

--- src/main/java/co/edu/unbosque/taller_3/ValidationServlet.java
+++ src/main/java/co/edu/unbosque/taller_3/ValidationServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.annotation.*;
 @WebServlet(name = "validationServlet", value = "/validation-servlet")
 public class ValidationServlet extends HttpServlet {
     private String message;
+    private String UPLOAD_DIRECTORY = "DBfiles";
 
     public void init() {
         message = "Ingreso exitoso";
@@ -15,10 +16,18 @@ public class ValidationServlet extends HttpServlet {
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
 
+        String uploadPath = getServletContext().getRealPath("") + File.separator + UPLOAD_DIRECTORY;
+
+        String rol="";
+        if(rol.equals("propietario")){
+            response.sendRedirect(request.getContextPath() + "/propietario.html");
+        }else if(rol.equals("funcionario")){
+            response.sendRedirect(request.getContextPath() + "/admin.html");
+        }
         // Hello
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
+        out.println("<h1>" + message+ "</h1>");
         out.println("</body></html>");
     }
 
